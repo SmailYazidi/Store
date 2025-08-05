4282344f25bd847d0a208b95c2e910e995ebcf24
smailyazidi <smailyazidi@gmail.com>
2025-08-05 20:33:50 +0100

    Store

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,12 +1,50 @@
 "use client"
 
-import React, { useState } from "react"
+import React, { useState,useEffect } from "react"
 import AdminSidebar from "@/components/admin/sidebar"
 import AdminHeader from "@/components/admin/header"
-import AdminMain from "@/components/admin/main"
-
+import { useRouter, usePathname } from "next/navigation"
+import AdminDashboard from "@/components/admin/main/page"
+import Loading from '@/components/Loading';
 export default function Admin() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+ const router = useRouter()
+  const pathname = usePathname()
+  const [loading, setLoading] = useState(true)
+
+
+
+
+  useEffect(() => {
+    // Skip auth check for login page
+    if (pathname === "/admin/login") {
+      setLoading(false)
+      return
+    }
+
+    // Check authentication
+    const checkAuth = async () => {
+      try {
+        const response = await fetch("/api/admin/account")
+        if (!response.ok) {
+          router.push("/admin/login")
+          return
+        }
+        setLoading(false)
+      } catch (error) {
+        router.push("/admin/login")
+      }
+    }
+
+    checkAuth()
+  }, [pathname, router])
+
+
+  if (loading) {
+    return (
+  <Loading />
+    )
+  }
 
   return (
     <div className="relative min-h-screen flex flex-col">
@@ -25,8 +63,16 @@ export default function Admin() {
 
       <div className="flex-1 flex flex-col">
         <AdminHeader onMenuClick={() => setIsSidebarOpen(prev => !prev)} />
-        <AdminMain/>
+        <main className="p-4 pt-25 bg-white text-black min-h-screen">
+
+        <AdminDashboard/>
+      </main>
       </div>
     </div>
   )
 }
+
+
+
+
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,59 @@
 "use client"
 
-import React, { useState } from "react"
+import React, { useState,useEffect } from "react"
 import ClientSidebar from "@/components/client/sidebar"
 import ClientHeader from "@/components/client/header"
-import ClientMain from "@/components/client/main"
+import CategorySection from "@/components/client/main/CategorySection"
+import { Category, Product } from "@/lib/models"
+import { toast } from "sonner"
+import Loading from '@/components/Loading';
+
 
 export default function Home() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+  const [categories, setCategories] = useState<Category[]>([])
+  const [products, setProducts] = useState<Product[]>([])
+  const [loading, setLoading] = useState(true)
+
+
+ useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [catRes, prodRes] = await Promise.all([
+          fetch("/api/client/categories"),
+          fetch("/api/client/products"),
+        ])
+
+        if (!catRes.ok || !prodRes.ok) throw new Error("فشل في جلب البيانات")
+
+        const [catData, prodData] = await Promise.all([
+          catRes.json(),
+          prodRes.json(),
+        ])
+
+        setCategories(catData)
+        setProducts(prodData)
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          toast.error(error.message)
+        } else {
+          toast.error("حدث خطأ أثناء جلب البيانات")
+        }
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchData()
+  }, [])
+
+  if (loading) {
+    return (
+  <Loading />
+    )}
+
+
+
 
   return (
     <div className="relative min-h-screen flex flex-col">
@@ -25,7 +72,23 @@ export default function Home() {
 
       <div className="flex-1 flex flex-col">
         <ClientHeader onMenuClick={() => setIsSidebarOpen(prev => !prev)} />
-        <ClientMain />
+          <main className="p-4 pt-25 bg-white text-black min-h-screen">
+      {categories.map((category) => {
+        const categoryProducts = products.filter(
+          (product) => product.categoryId === category._id && product.isVisible
+        )
+
+        if (!category || !category.name || categoryProducts.length === 0) return null
+
+        return (
+          <CategorySection
+            key={category._id}
+            category={category}
+            products={categoryProducts.slice(0, 6)}
+          />
+        )
+      })}
+    </main>
       </div>
     </div>
   )
