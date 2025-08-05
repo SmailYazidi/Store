6521ff90f573cb494bed940144253d10ed1a363d
smailyazidi <smailyazidi@gmail.com>
2025-08-05 16:29:07 +0100

    Store

--- components/admin/admin-layout.tsx
+++ components/admin/admin-layout.tsx
@@ -57,7 +57,7 @@ export function AdminLayout({ children }: AdminLayoutProps) {
   }
 
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen">
       <AdminSidebar open={sidebarOpen} onOpenChange={setSidebarOpen} />
 
       <div className="lg:pl-64">
